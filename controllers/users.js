5eb558ba84aa74d13c078712c75f4c0ff920b7ce
Arhci <arlan.abkeyev@mail.ru>
2023-09-29 03:52:45 +0300

    Database connection

---
+++ controllers/users.js
@@ -0,0 +1,45 @@
+const {prisma} = require('../prisma/prisma-client')
+const brypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
+
+@route
+@desc
+@access
+
+const login = async (req, res) => {
+    const {email, password} = req.body;
+
+    if (!email && password) {
+        return res.status(400).json({message: 'Пожалуйста, заполните обязательные все поля'})
+    }
+
+    const user = await prisma.user.findFirst({
+        where: {
+            email,
+        }
+    });
+
+    const isPasswordCorrect = user && (await bryp.compare(password, user.password));
+    if (user && isPasswordCorrect) {
+        res.status(200).json({
+            id: user.id,
+            email: user.email,
+            name: user.name
+        })
+    } else {
+        return res.status(400).json({message: 'Неверно введен логин или пароль'})
+    }
+}
+const register = async (req, res) => {
+    res.send('register');
+}
+const current = async (req, res) => {
+    res.send('current');
+}
+
+
+module.exports = {
+    login,
+    register,
+    current
+}
